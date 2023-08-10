5cdf5721d101990b1389ebb615c224cb0b8c055b
Marcos-mrd <131389602+Marcos-mrd@users.noreply.github.com>
2023-08-09 22:07:30 -0300

    site _Js

---
+++ site01/js/tringulo.js
@@ -0,0 +1,17 @@
+// calcular area!!
+
+var base;
+var autura;
+var lado;
+
+//calculo area!
+
+function quadradoA(){
+    let calculoA =  (base * h);
+}
+
+
+//calculo perimetro
+function quadradoP(){
+    let calculoP = (4 *lado);
+}
