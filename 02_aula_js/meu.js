daf9303c178bdafab6008961bc88adfcd65254f1
Marcos rian <marcosrian.mrd@gmail.com>
2023-08-17 19:12:42 -0300

    adiciona_tabuada

---
+++ 02_aula_js/meu.js
@@ -0,0 +1,8 @@
+function soma (){
+    var n1 = parseInt(document.querySelector("n1").value);
+    var n2 = parseInt(document.querySelector("n2").value);
+
+    let resultado = n1 + n2;
+
+    alert(`A soma entre os numeros: ${soma}`);
+}
