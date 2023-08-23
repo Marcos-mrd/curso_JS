1b266e0c9bf6ba15616df7b83116053aa6f915b8
Marcos rian <marcosrian.mrd@gmail.com>
2023-08-23 20:49:34 -0300

    add

---
+++ Lost Puppy Finder/mudar_cor.js
@@ -0,0 +1,32 @@
+
+// var cor = "fundo_principal"
+
+// function mudar_cor(){
+
+//     let corpo  =  document.querySelector("#cor_fundo")
+//     let corselecionada = corpo.className
+
+
+//     console.log(corpo)
+//     //console.log(corselecionada)
+
+//     if(cor == corselecionada){
+//         corpo.classList.remove("fundo_principal")
+//         corpo.classList.add("fundo_cor") 
+
+//     }
+//     else{
+//         corpo.classList.remove("fundo_cor")
+//         corpo.classList.add("fundo_principal")
+//     }
+   
+    
+    
+    
+// }
+
+var cor_body = cor_fundo;
+
+    function mudar_cor(){
+        if( cor_body != "brown" )
+    }
